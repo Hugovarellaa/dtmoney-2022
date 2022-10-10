a6b11bd25a32d2ce17e92d9a3f8ce327d3aaded2
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-10 12:18:30 -0300

    Modal

--- dt-money/src/components/Header/index.tsx
+++ dt-money/src/components/Header/index.tsx
@@ -1,12 +1,23 @@
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from '../NewTranstionModal';
 import { ButtonTransaction, HeaderContainer, HeaderContent } from "./styles";
+
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <ButtonTransaction>Nova transação</ButtonTransaction>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <ButtonTransaction>Nova transação</ButtonTransaction>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+
+        </Dialog.Root>
+
       </HeaderContent>
     </HeaderContainer>
   )
