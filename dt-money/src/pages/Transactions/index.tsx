f9a61e96d516b154fba62ff2af53e19c96cf35c1
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-10 10:33:45 -0300

    Create table in transaction

--- dt-money/src/pages/Transactions/index.tsx
+++ dt-money/src/pages/Transactions/index.tsx
@@ -1,12 +1,43 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
-import { TransactionsContainer } from "./styles";
+import { PriceHighLight, TransactionsContainer, TransactionTable } from "./styles";
 
-export function Transactions(){
+export function Transactions() {
   return (
-    <TransactionsContainer>
+    <div>
+
       <Header />
       <Summary />
-    </TransactionsContainer>  
+
+      <TransactionsContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">
+                  R$ 12.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td>Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">
+                  - R$ 59,00
+                </PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+
+          </tbody>
+        </TransactionTable>
+      </TransactionsContainer>
+    </div>
+
   )
 } 
