0007b1e8ca9c0ecd51c0ba811e0caf18642f51be
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-11 11:10:32 -0300

    Logica de buscar no search input

--- dt-money/src/pages/Transactions/components/SearchForm/index.tsx
+++ dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,45 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import * as zod from "zod";
+import { useTransaction } from "../../../../hooks/useTransaction";
 import { SearchFormContainer } from "./styles";
 
+const searchFormSchema = zod.object({
+  query: zod.string()
+})
+
+type SearchFormInputs = zod.infer<typeof searchFormSchema>
+
 export function SearchForm() {
+  const {fetchTransactions} = useTransaction()
+  
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+    defaultValues: {
+      query: ''
+    }
+  })
+
+  async function handleSearchTransaction(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+
+  }
+
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
-      <button type="submit">
-        <MagnifyingGlass size={20}/>
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransaction)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
         Buscar
       </button>
     </SearchFormContainer>
