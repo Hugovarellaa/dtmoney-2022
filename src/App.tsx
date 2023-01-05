a57453eb15ee151968baaa3d786035b8081cb6ab
hugo <hugovarellaa@gmail.com>
2023-01-05 11:02:00 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from "styled-components";
+import { Home } from "./pages/Home";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/theme/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>First render</h1>
+      <Home />
 
       <GlobalStyles />
     </ThemeProvider>
