57a7b202301cf6d45765e789fb8d1ac8d154b1fc
hugo <hugovarellaa@gmail.com>
2023-01-05 13:08:50 -0300

    Layout modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
+import * as Dialog from "@radix-ui/react-dialog";
 import { Logo } from "../Logo";
+import { NewTransactionModal } from "../NewTransactionModal";
 import { HeaderButton, HeaderContainer, HeaderContent } from "./styles";
 
 export function Header() {
@@ -6,7 +8,14 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <Logo />
-        <HeaderButton>Nova transação</HeaderButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <HeaderButton>Nova transação</HeaderButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
